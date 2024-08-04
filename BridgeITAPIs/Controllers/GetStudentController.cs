43d3db8dc04211a228f19282e580b2e06242d40e
Faizan <mfaizaan771@gmail.com>
2024-08-04 17:57:40 +0500

    Search Student by Id API integrated

---
+++ BridgeITAPIs/Controllers/GetStudentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BridgeITAPIs.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace BridgeITAPIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetStudentController : ControllerBase
+    {
+        private readonly BridgeItContext _dbContext;
+        public GetStudentController(BridgeItContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("student-by-id/{userId}")]
+        public async Task<IActionResult> GetStudent(Guid userId)
+        {
+            var student = await _dbContext.Students
+                .Include(s => s.User)
+                .Include(s => s.University)
+                //.Include(s => s.Skills)
+                .FirstOrDefaultAsync(s => s.Id == userId);
+
+            if (student == null)
+            {
+                return NotFound("Student not found.");
+            }
+
+            var dto = new GetStudentDTO
+            {
+                FirstName = student.User?.FirstName ?? string.Empty,
+                LastName = student.User?.LastName ?? string.Empty,
+                Email = student.User?.Email ?? string.Empty,
+                //Skills = student.Skills.Select(s => s.Skill1).ToList(),
+                ImageData = student.User?.ImageData ?? string.Empty,
+                UniversityName = student.University?.Name ?? string.Empty,
+                Address = student.University?.Address ?? string.Empty,
+                RollNumber = student?.RollNumber.ToString() ?? string.Empty
+            };
+
+            return Ok(dto);
+        }
+
+    }
+}
--- BridgeITAPIs/DTOs/GetStudentDTO.cs
+++ BridgeITAPIs/DTOs/GetStudentDTO.cs
@@ -1,19 +1,13 @@
 ﻿namespace BridgeITAPIs.DTOs;
 
-public class RegisterStudentDTO
+public class GetStudentDTO
 {
     public string FirstName { get; set; } = string.Empty;
-
     public string LastName { get; set; } = string.Empty;
-
     public string Email { get; set; } = string.Empty;
-
-    public string Role { get; set; } = string.Empty;
     public List<string> Skills { get; set; } = new List<string>();
-
-    public string Password { get; set; } = string.Empty;
-
-    public int? RollNumber { get; set; } = null;
-
-    public Guid UniversityId { get; set; }
+    public string ImageData { get; set; } = string.Empty;
+    public string UniversityName { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string RollNumber { get; set; } = string.Empty;
 }
