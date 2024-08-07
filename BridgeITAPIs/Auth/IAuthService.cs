a66128ee3dd123f8fb9497735130881f40edf18f
Faizan <mfaizaan771@gmail.com>
2024-08-07 23:48:27 +0500

    login and other apis are implemented(role based yet to be implemented)

---
+++ BridgeITAPIs/Auth/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿using BridgeITAPIs.DTOs;
+
+namespace BridgeITAPIs.Auth;
+
+public interface IAuthService
+{
+    Task<string> AuthenticateAsync(UserDTO userDTO);
+}
---
+++ BridgeITAPIs/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BridgeITAPIs.Auth;
+namespace BridgeITAPIs.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] UserDTO userDTO)
+    {
+        try
+        {
+            var token = await _authService.AuthenticateAsync(userDTO);
+            return Ok(new { Token = token });
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            return Unauthorized(ex.Message);
+        }
+    }
+}
