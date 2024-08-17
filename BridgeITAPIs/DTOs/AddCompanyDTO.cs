6c895b72319cead2115031c005f8f669c403d832
Faizan <mfaizaan771@gmail.com>
2024-08-17 22:04:42 +0500

    university & students api modified, company api added

---
+++ BridgeITAPIs/DTOs/AddCompanyDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BridgeITAPIs.DTOs
+{
+    public class AddCompanyDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Business { get; set; } = string.Empty;
+    }
+}
---
+++ BridgeITAPIs/DTOs/EditStudentNameRollnumberDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BridgeITAPIs.DTOs
+{
+    public class EditStudentNameRollnumberDTO
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string RollNumber { get; set; } = string.Empty;
+    }
+}
