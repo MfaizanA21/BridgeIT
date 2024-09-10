f6c08060b6742ce956df912c6b23187afe5bd897
Faizan <mfaizaan771@gmail.com>
2024-09-10 21:02:55 +0500

    proposal api for expert created

--- BridgeITAPIs/DTOs/ProjectProposalDTOs/SendProposalDTO.cs
+++ BridgeITAPIs/DTOs/ProjectProposalDTOs/SendProposalDTO.cs
@@ -3,7 +3,7 @@
     public class SendProposalDTO
     {
         public string proposal { get; set; } = string.Empty;
-        public string status { get; set; } = string.Empty;
+        //public string status { get; set; } = string.Empty;
         public Guid studentId { get; set; }
         public Guid projectId { get; set; }
     }
