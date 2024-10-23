31fd341d7d18c765d3a4f739f0682d1df6ab4f2e
Faizan <mfaizaan771@gmail.com>
2024-10-24 00:30:01 +0500

    Otp generate/regenerate and send APIs are implemented and tested.

---
+++ BridgeITAPIs/Controllers/OtpController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BridgeITAPIs.DTOs.OtpDTOs;
+
+namespace BridgeITAPIs.Controllers;
+
+[Route("api/otp")]
+[ApiController]
+public class OtpController : ControllerBase
+{
+    private readonly BridgeItContext _context;
+
+    public OtpController(BridgeItContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("generate-otp")]
+    public async Task<IActionResult> GenerateOtp([FromBody] string email)
+    {
+        if (string.IsNullOrEmpty(email))
+        {
+            return BadRequest("Email is required.");
+        }
+
+        var otp = new Otp
+        {
+            email = email,
+            otp = new Random().Next(100000, 999999),
+            created_at = DateTime.UtcNow,
+        };
+
+        _context.Set<Otp>().Add(otp);
+        await _context.SaveChangesAsync();
+
+        return Ok(otp);
+    }
+
+    [HttpPost("verify-otp")]
+    public async Task<IActionResult> VerifyOtp([FromBody] VerifyOtpDTO otp)
+    {
+        var otpData = await _context
+            .Set<Otp>()
+            .FirstOrDefaultAsync(o => o.email == otp.email && o.otp == otp.otp);
+
+        if (otp == null)
+        {
+            return BadRequest("Otp data is null.");
+        }
+
+        if (otp.otp != otpData.otp)
+        {
+            return BadRequest("Invalid OTP.");
+        }
+
+        if (otp.email != otpData.email)
+        {
+            return BadRequest("Invalid OTP.");
+        }
+
+        if (otpData == null)
+        {
+            return BadRequest("Invalid OTP.");
+        }
+
+        if (DateTime.UtcNow.Subtract(otpData.created_at).TotalMinutes > 5)
+        {
+            return BadRequest("OTP expired.");
+        }
+
+        return Ok("OTP verified successfully.");
+    }
+
+    [HttpPatch("regenerate-otp")]
+    public async Task<IActionResult> ReGenerateOtp([FromBody] string email)
+    {
+        if (string.IsNullOrEmpty(email))
+        {
+            return BadRequest("Email is required.");
+        }
+
+        var otpData = await _context
+            .Set<Otp>()
+            .FirstOrDefaultAsync(o => o.email == email);
+
+        if (otpData == null)
+        {
+            return BadRequest("Otp data not found/email doesnot exits.");
+        }
+
+        int new_otp = new Random().Next(100000, 999999);
+
+        otpData.otp = new_otp;
+
+        DateTime time_now = DateTime.UtcNow;
+
+        otpData.created_at = time_now;
+
+        await _context.SaveChangesAsync();
+        return Ok(new_otp);
+    }
+
+}
---
+++ BridgeITAPIs/DTOs/OtpDTOs/VerifyOtpDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BridgeITAPIs.DTOs.OtpDTOs
+{
+    public class VerifyOtpDTO
+    {
+        public string email { get; set; }
+        public int otp { get; set; }
+    }
+}
