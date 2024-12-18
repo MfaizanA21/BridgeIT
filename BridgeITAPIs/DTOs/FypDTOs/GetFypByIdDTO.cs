864f8ecfb42216aac7bb3fbeca80f134e72453f7
MfaizanA21 <mfaizaan771@gmail.com>
2024-12-18 23:48:46 +0500

    Approve Reject API for FYP and Get FYP Api by Id

---
+++ BridgeITAPIs/DTOs/FypDTOs/GetFypByIdDTO.cs
@@ -0,0 +1,14 @@
+namespace BridgeITAPIs.DTOs.FypDTOs;
+
+public class GetFypByIdDTO
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string FypId { get; set; } = string.Empty;
+    public int? Members { get; set; }
+    public string Batch { get; set; } = string.Empty;
+    public string Technology { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    
+}
