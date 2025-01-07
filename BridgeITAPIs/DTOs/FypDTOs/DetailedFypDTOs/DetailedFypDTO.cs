6b3d9e968900849f24c291cdd2d41f7dbb9b0243
MfaizanA21 <mfaizaan771@gmail.com>
2025-01-07 18:20:38 +0500

    Detailed fYP DTO implemented

---
+++ BridgeITAPIs/DTOs/FypDTOs/DetailedFypDTOs/DetailedFypDTO.cs
@@ -0,0 +1,15 @@
+namespace BridgeITAPIs.DTOs.FypDTOs.DetailedFypDTOs;
+
+public class DetailedFypDTO
+{
+    public Guid Id { get; set; }
+    public string? Title { get; set; }
+    public int? Members { get; set; }
+    public string? Batch { get; set; }
+    public string? Technology { get; set; }
+    public string? Description { get; set; }
+    public string? Status { get; set; }
+
+    public FacultyDTO? Faculty { get; set; }
+    public List<StudentDTO> Students { get; set; } = new List<StudentDTO>();
+}
---
+++ BridgeITAPIs/DTOs/FypDTOs/DetailedFypDTOs/FacultyDTO.cs
@@ -0,0 +1,10 @@
+namespace BridgeITAPIs.DTOs.FypDTOs.DetailedFypDTOs;
+
+public class FacultyDTO
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Interest { get; set; }
+    public string? Department { get; set; }
+    public string? Post { get; set; }
+}
