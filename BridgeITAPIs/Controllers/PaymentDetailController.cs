ca91247d5b8f6892e39d1f889f7083d6c0763003
MfaizanA21 <mfaizaan771@gmail.com>
2025-05-02 21:57:20 +0500

    payment details apis added

--- BridgeITAPIs/Controllers/PaymentDetailController.cs
+++ BridgeITAPIs/Controllers/PaymentDetailController.cs
@@ -1,3 +1,4 @@
+using BridgeITAPIs.DTOs.PaymentDTOs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,7 +15,7 @@ public class PaymentDetailController : Controller
         _dbContext = dbContext;
     }
     
-    [HttpGet("get-payment-details")]
+    [HttpGet("payment-details")]
     public async Task<IActionResult> GetPaymentDetails()
     {
         var paymentDetails = await _dbContext.PaymentDetails
@@ -28,5 +29,40 @@ public class PaymentDetailController : Controller
         }
 
         return Ok(paymentDetails);
+    }
+    
+    [HttpGet("payment-details/{id}")]
+    public async Task<IActionResult> GetPaymentDetailById(Guid id)
+    {
+        var paymentDetail = await _dbContext.PaymentDetails
+            .Include(p => p.Project)
+            .ThenInclude(p => p!.Student)
+            .ThenInclude(s => s!.User)
+            .Include(p => p.Project)
+            .ThenInclude(p => p!.IndExpert)
+            .ThenInclude(i => i!.User)
+            .FirstOrDefaultAsync(p => p.ProjectId == id || p.Id == id);
+
+        if (paymentDetail == null)
+        {
+            return NotFound("No payment detail found for this project.");
+        }
+        
+        var paymentDetailDTO = new GetPaymentDetailsDTO
+        {
+            Id = paymentDetail.Id,
+            ProjectId = paymentDetail.ProjectId,
+            StudentName = paymentDetail.Project!.Student!.User!.FirstName + " " + paymentDetail.Project.Student.User.LastName,
+            ProjectOwnerName = paymentDetail.Project.IndExpert!.User!.FirstName + " " + paymentDetail.Project.IndExpert.User.LastName,
+            ProjectName = paymentDetail.Project.Title!,
+            StudentEmail = paymentDetail.Project.Student.User.Email!,
+            StudentPaymentAccountId = paymentDetail.Project.Student.StripeConnectId!,
+            PaidAt = paymentDetail.PaidAt,
+            Receipt = paymentDetail.PaymentSlip,
+            StudentId = paymentDetail.Project.Student.Id,
+            ProjectOwnerId = paymentDetail.Project.IndExpert.Id,
+        };
+
+        return Ok(paymentDetailDTO);
     }
 }
